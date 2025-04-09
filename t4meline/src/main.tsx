266c20eeaaec2b9b2d3af15d021aa351cc65d813
Oceane Bertin <oceane.bertin@unistra.fr>
2025-04-09 17:36:39 +0200

    fin de la page acceuil

--- t4meline/src/main.tsx
+++ t4meline/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import App from "./App.tsx";
 import Pioche from "./components/pioche.tsx";
 import Frise from "./components/frise.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Frise />
-    <Pioche />
+    <App />
   </StrictMode>
 );
