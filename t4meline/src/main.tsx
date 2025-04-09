5126c1f144d50ec029ce5e3821d8f5edfb2cba5c
Simon <simon.williams@etu.unistra.fr>
2025-04-09 11:51:28 +0200

    resolve import issue

--- t4meline/src/main.tsx
+++ t4meline/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Pioche from "./components/pioche.tsx";
-import Frise from "./components/Frise.tsx";
+import Frise from "./components/frise.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
