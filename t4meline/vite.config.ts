0b8afc848e8ea29525095f4c17e51de62837364c
KIGMOU Bilal <bilal.kigmou@etu.unistra.fr>
2025-04-11 00:25:26 +0200

    Update vite.config.ts

--- t4meline/vite.config.ts
+++ t4meline/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: '0.0.0.0', // Autorise les connexions externes
+    port: 3000,       // Définissez explicitement le port
+    open: true,       // Ouvre automatiquement le navigateur
+  },
 })
