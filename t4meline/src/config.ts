10d60b7ae9d8f663c2c03cc5612ef93d66b8fe92
Simon <simon.williams@etu.unistra.fr>
2025-04-11 09:10:58 +0200

    MMMMM

--- t4meline/src/config.ts
+++ t4meline/src/config.ts
@@ -1,6 +1,6 @@
 // config.ts
 import io from "socket.io-client";
 
-export const API_URL = "localhost:3001";
+export const API_URL = "https://w41-api.onrender.com/";
 export const socket = io(API_URL);
 //temp pour commit
